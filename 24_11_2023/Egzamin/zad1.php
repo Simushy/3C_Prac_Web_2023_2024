2e00d3dbada6fe23d2e92b935ac63ed0a58b73a2
Simushy <simushy@gmail.com>
2023-12-08 11:23:18 +0000

    fix code format

--- 24_11_2023/Egzamin/zad1.php
+++ 24_11_2023/Egzamin/zad1.php
@@ -5,16 +5,18 @@
   </head>
   <body style="background-color: black; color: white;text-align:center;margin-top:50px;">
   <a>Suma 100 parzystych liczb następujących po 100 wynosi:</a>
-    <?php $a = 100;
-    $licznik = 0;
-    $suma = 0;
-    while ($licznik < 100) {
-      $a++;
-      if ($a % 2 == 0) {
-        $licznik++;
-        $suma += $a;
+    <?php
+      $a = 100;
+      $licznik = 0;
+      $suma = 0;
+      while ($licznik < 100) {
+        $a++;
+        if ($a % 2 == 0) {
+          $licznik++;
+          $suma += $a;
+        }
       }
-    }
-    print_r($suma); ?>
+      print_r($suma);
+    ?>
   </body>
 </html>
